8575c3f0d913c2225622c16901269ddfb157b637
Sudhanshu <211230053@nitdelhi.ac.in>
2023-08-12 21:55:47 +0530

    Variables

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+import(
+	"fmt"
+)
+var print = fmt.Println
+func main(){
+	var vName string = "Sudhanshu"
+	var vSurname = "Shrivastava"
+	print(vName,vSurname)
+	var v1, v2 = 1,2
+	print("v1 and v2 : " ,v1,"and",v2)
+	// methods to initialize variables in GO
+}
